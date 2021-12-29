3cabd689b3a6ae3a2f524fc17e184037807b3549
Aniketsomkuwar <aniketsomkuwar1101@gmail.com>
2021-12-30 02:50:17 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -5,45 +5,47 @@ $(function () {
     $(".left .emoji").addClass(
       "animate__animated animate__tada animate__infinite"
     );
-
   });
-
-  $("#Rock1").click(function () { 
-      game("Rock");
-      
+  $("#playagain").hide();
+  $("#Rock1").click(function () {
+    game("Rock");
   });
-  $("#Paper1").click(function () { 
+  $("#Paper1").click(function () {
     game("Paper");
-    
-});
-$("#Scissor1").click(function () { 
+  });
+  $("#Scissor1").click(function () {
     game("Scissor");
-    
-});
+  });
+
   function game(player) {
     var computer_values = ["Rock", "Paper", "Scissor"];
-    var randvalue = computer_values[Math.floor(Math.random() * computer_values.length)];
-    $("#"+randvalue+"2 img" ).css('margin-right', '90px');
-    $("#"+player+"1 img" ).css('margin-left', '90px');
-    $(".left .emoji").removeClass("animate__animated animate__tada animate__infinite");
-    console.log("player -> "+player + " |||| computer -> "+randvalue);
+    var randvalue =
+      computer_values[Math.floor(Math.random() * computer_values.length)];
+    $("#player2").text(randvalue);
+    $("#player1").text(player);
+
+    $(".left .emoji").removeClass(
+      "animate__animated animate__tada animate__infinite"
+    );
+    console.log("player -> " + player + " |||| computer -> " + randvalue);
     if (player == randvalue) {
-      console.log(true);
       $("#play").text("tie");
-      return;
-   
+    } else if (player == "Rock" && randvalue == "Scissor") {
+      $("#play").text("You Won");
+    } else if (player == "Paper" && randvalue == "Rock") {
+      $("#play").text("You Won");
+    } else if (player == "Scissor" && randvalue == "Paper") {
+      $("#play").text("You Won");
+    } else {
+      $("#play").text("You Lose");
     }
-    if(player=="Rock" && randvalue=="Scissor"){
-        $("#play").text("You Won");
-        
-    }
-   else if(player=="Paper" && randvalue=="Rock"){
-        $("#play").text("You Won");
-    }
-   else if(player=="Scissor" && randvalue=="Paper"){
-        $("#play").text("You Won");
-    }else{
-        $("#play").text("You Lose");
-    }
+    waitTime();
+    $("#playagain").show();
+  }
+
+  function waitTime() {
+    $("#playagain").click(function () {
+      location.reload();
+    });
   }
 });
